b5144c2c7a26ec9bafc0402b3fe31b1d64a225bc
DeveloperSubhadeep1 <161279108+DeveloperSubhadeep1@users.noreply.github.com>
2024-04-27 09:49:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,18 @@
 import os, re
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-OWNER = int(os.environ.get("OWNER", ""))
-BOT_USERNAME = os.environ.get('BOT_USERNAME', "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7103264571:AAHQABT_b98LJ_tklhwsTLSaPU6OibhTocE")
+API_ID = int(os.environ.get("API_ID", "27972068"))
+API_HASH = os.environ.get("API_HASH", "6e7e2f5cdddba536b8e603b3155223c1")
+OWNER = int(os.environ.get("OWNER", "6075512585"))
+BOT_USERNAME = os.environ.get('BOT_USERNAME', "Devil Renamer 4 GB")
 
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "HxBots")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002031041556"))
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002140933136"))
 
-DB_URL = os.environ.get("DB_URL", "")
-DB_NAME = os.environ.get("DB_NAME", "renamebot-premium")
+DB_URL = os.environ.get("DB_URL", "Cluster0")
+DB_NAME = os.environ.get("DB_NAME", "mongodb+srv://text1:text1@cluster0.0lnolxg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+ 
 
 STRING = os.environ.get("STRING", "")
 BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/cca849a2f63053fa3f622.jpg")
